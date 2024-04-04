cee6cc91039ccaf7774ef4516129719ddd379b61
Yash <yashshah035@gmail.com>
2024-04-04 19:15:57 +0530

    invalid input modification

--- hr_questions.py
+++ hr_questions.py
@@ -36,20 +36,17 @@ def generate_questions(categories):
             questions.extend(hr_question_database[category])
     return questions
 
-
 print("Categories of HR questions available:")
 category_list = list(hr_question_database.keys())
 for i, category in enumerate(category_list, start=1):
     print(f"{i}. {category.capitalize()}")
 
 while True:
-   
-    remaining_categories = category_list.copy()  
-
+    remaining_categories = category_list.copy()
     print("\nEnter the category number for HR questions ('q' to quit):")
     category_input = input()
-
     if category_input.lower() == 'q':
+        print("Exiting program...")
         break
 
     selected_categories = []
@@ -59,14 +56,17 @@ while True:
             if 0 <= category_index < len(remaining_categories):
                 selected_category = remaining_categories.pop(category_index)
                 selected_categories.append(selected_category)
+            else:
+                print(f"Invalid input: {number}")
         except ValueError:
             print(f"Invalid input: {number}")
 
+    if not selected_categories:
+        print("Please Enter Valid Input From Categories.")
+        continue
     questions = generate_questions(selected_categories)
     print("\nCommon HR Interview Questions:")
     for question in questions:
         print("- " + question)
 
-    if not selected_categories:
-        print("All categories covered. Exiting...")
-        break
+    
