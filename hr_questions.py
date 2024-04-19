fc576d5f6e5454fb1086db22268b31646e460617
Darshula <tanmayvijaykulkarni2002@gmail.com>
2024-04-19 23:30:22 +0530

    Moved logic to class

--- hr_questions.py
+++ hr_questions.py
@@ -1,48 +1,42 @@
 import json
+import os
+import random
 
-hr_question_database = {}
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+question_database: dict[str, list[str]] = {}
 
 with open('questions.json') as questions:
-    hr_question_database = json.load(questions)
+    question_database = json.load(questions)
 
-
-def generate_questions(categories):
-    questions = []
-    for category in categories:
-        if category in hr_question_database:
-            questions.extend(hr_question_database[category])
-    return questions
-
-
-print("Categories of HR questions available:")
-category_list = list(hr_question_database.keys())
-for i, category in enumerate(category_list, start=1):
-    print(f"{i}. {category.capitalize()}")
-
-while True:
-    remaining_categories = category_list.copy()
-    print("\nEnter the category number for HR questions ('q' to quit):")
-    category_input = input()
-    if category_input.lower() == 'q':
-        print("Exiting program...")
-        break
-
-    selected_categories = []
-    for number in category_input.split():
-        try:
-            category_index = int(number) - 1
-            if 0 <= category_index < len(remaining_categories):
-                selected_category = remaining_categories.pop(category_index)
-                selected_categories.append(selected_category)
+if __name__ == '__main__':
+    selected_questions = {}
+    for category in question_database.keys():
+        # Selecting 2 random questions per category from `questions database`
+        selected_questions.update(
+            {category: random.sample(question_database[category], 3)})
+    previous_output = ''
+    while True:
+        clear_screen()
+        print(previous_output, '\n')
+        if not len(question_database.keys()):
+            print('All questions have been displayed.')
+            break
+        print('Enter the category number for HR questions ' +
+              '(\'q\' to quit): ')
+        for i, category in enumerate(question_database.keys()):
+            print(f'{i+1}: {category}')
+        user_input = input()
+        if user_input.isalpha():
+            if user_input == 'q':
+                break
             else:
-                print(f"Invalid input: {number}")
-        except ValueError:
-            print(f"Invalid input: {number}")
-
-    if not selected_categories:
-        print("Please Enter Valid Input From Categories.")
-        continue
-    questions = generate_questions(selected_categories)
-    print("\nCommon HR Interview Questions:")
-    for question in questions:
-        print("- " + question)
+                continue
+        else:
+            user_input = int(user_input)
+        if user_input in range(len(question_database.items())+1):
+            previous_output = '\n'.join(question_database.pop(
+                list(question_database.keys())[user_input-1]))
